ca43e8e3017e6d7170da813c8ec964dc96e46f7c
Henrique Kubinha <rickubinhetz@gmail.com>
2024-02-02 20:01:03 -0300

    Initial commit

---
+++ exercises.py
@@ -0,0 +1,14 @@
+# This is the core script for a series of exercises intended on getting the hang of the main excel-related
+# libraries existing in Python.
+
+# -------------------------------------- Exercises --------------------------------------
+# 1) Create a new Excel file and write it on a specific cell;
+# 2) Read several specific cells from an existing Excel file and store them on variables;
+# 3) Read several columns and iterate through them.
+# 4) Find the index of a cell
+# 5) Select and read a cell from a given index
+# 6) Manipulate an opened sheet
+# 7) Learn to overwrite and append on an existing, non-empty, cell;
+# 8) Learn to format cells;
+# 9) Finally, save something at an Excel Sheet and run a VBA macro from there, that then runs another py script.
+
