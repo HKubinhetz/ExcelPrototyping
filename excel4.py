ef0776dc5fe5f71434b6f35d321e8985964c3b12
Henrique Kubinhetz <rickubinhetz@gmail.com>
2024-02-04 18:51:43 -0300

    Exercise 3 and 4

---
+++ excel4.py
@@ -0,0 +1,34 @@
+# -------------------------------------- Exercises --------------------------------------
+# 4) Find the index of a cell
+# My feeling is we ought start by using pandas right away
+# Update: my feeling was right! :)
+
+# Imports
+import pandas as pd
+import numpy as np
+
+# Read Excel Sheet
+df = pd.read_excel(".\\examplesheet\\example.xlsx")
+
+# The following columns will compose our desired indexes.
+columnlist = ['INSTALAÇÃO', 'NOME/SOBRENOME', 'Lacunas']
+indexlist = []
+
+# Finding the index of a column - columns.get_loc function.
+# Iterating through those and building a list.
+for columnlabel in columnlist:
+    found_index = df.columns.get_loc(columnlabel)
+    indexlist.append(found_index)
+
+# Finding the index of a row
+row_index = np.where(df['NOME/SOBRENOME'] == "AGC VIDROS DO BRASIL LTDA.")[0][0]
+
+# Creating a list with the desired information:
+datalist = []
+for i in range(len(indexlist)):
+    info = df.iloc[row_index, indexlist[i]]
+    datalist.append(info)
+
+# Printing the final result
+print(f"O cliente {datalist[0]} - {datalist[1]} possui {datalist[2]} lacunas;")
+
--- exercises.py
+++ exercises.py
@@ -3,11 +3,11 @@
 
 # -------------------------------------- Exercises --------------------------------------
 # COMPLETE - 1) Create a new Excel file and write it on a specific cell;
-# 2) Read several specific cells from an existing Excel file and store them on variables;
-# 3) Read several columns and iterate through them.
+# COMPLETE - 2) Read several specific cells from an existing Excel file and store them on variables;
+# COMPLETE - 3) Read several columns and iterate through them.
 # 4) Find the index of a cell
 # 5) Select and read a cell from a given index
-# 6) Manipulate an opened sheet
+# 6) Manipulate an opened sheet (That requires some go-around)
 # 7) Learn to overwrite and append on an existing, non-empty, cell;
 # 8) Learn to format cells;
 # 9) Finally, save something at an Excel Sheet and run a VBA macro from there, that then runs another py script.
